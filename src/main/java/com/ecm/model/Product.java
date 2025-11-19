cc5507e39ab07b10ea1654c59341a5b041f2ac7e
Devansh Sharma <devanshsharma812@gmail.com>
2025-11-19 12:59:11 +0530

    Entity commit

--- src/main/java/com/ecm/model/Product.java
+++ src/main/java/com/ecm/model/Product.java
@@ -1,9 +1,18 @@
 package com.ecm.model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -35,6 +44,25 @@ public class Product {
 	private int quantity;
 	
 	private String colour;
+	
+	@ElementCollection
+	private List<String> images = new ArrayList<String>();
+	
+	private int numRatings;
+	
+	@ManyToAny
+	private Category category;
+	
+	@ManyToAny
+	private Seller seller;
+	
+	private LocalDateTime createdAt;
+	
+	private String sizes;
+
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Review> reviews = new ArrayList<>();
+	
 	
 	
 }
