d5ff641763b71a4e5261a93fd74a3828c36fc143
Devansh Sharma <devanshsharma812@gmail.com>
2025-11-21 20:00:58 +0530

    Auth Setup

--- src/main/java/com/ecm/Controller/AuthController.java
+++ src/main/java/com/ecm/Controller/AuthController.java
@@ -1,15 +1,33 @@
 package com.ecm.Controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecm.model.User;
+import com.ecm.repository.UserRepository;
+import com.ecm.response.SignupRequest;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
 	
-	public ResponseEntity<User> createUserHandler(){
-		return null;
+	private final UserRepository userRepository;
+	
+	
+	@PostMapping("/signup")
+	public ResponseEntity<User> createUserHandler(@RequestBody SignupRequest req){
+		User user = new User();
+		user.setEmail(req.getEmail());
+		user.setFullName(req.getFullName());
+		
+		User saveUser = userRepository.save(user);
+		return ResponseEntity.ok(saveUser);
 	}
 
 }
--- src/main/java/com/ecm/response/SignupRequest.java
+++ src/main/java/com/ecm/response/SignupRequest.java
@@ -1,5 +1,8 @@
 package com.ecm.response;
 
+import lombok.Data;
+
+@Data
 public class SignupRequest {
 
 	private String email;
